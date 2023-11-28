b36eacec613f3bba83e533f0dde21154d70a6f87
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2023-11-28 14:42:44 +0300

    Changed input data

---
+++ FileReader/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FileReader
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            try
+            {
+                var logger = new Logger(Constants.LOGFILEPATH);
+                var words = await TextHandler.PreprocessTextAsync(Constants.FILEPATH);
+                var entries = TextHandler.CountWords(words);
+                var sorted = TextHandler.SortResults(entries);
+
+                await Writer.WriteFile(Constants.OUTPUTFILEPATH, sorted);
+                await logger.LogAsync("Результаты успешно записаны в файл ResultPairs.txt");
+
+                DisplayResults(sorted);
+            }
+            catch (Exception ex)
+            {
+                await Constants.logger.LogAsync($"Произошла ошибка: {ex.Message}");
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                throw new Exception(ex.Message);
+            }
+        }
+
+        static void DisplayResults(LinkedList<WordCountEntry> entries)
+        {
+            Console.WriteLine("Статистика слов:");
+            entries.ForEach(x => Console.WriteLine($"{x.Word}: {x.Count}"));
+        }
+    }
+}
