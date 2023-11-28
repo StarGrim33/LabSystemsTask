a46c3e28935e37091e424e6da5b234eae6f7ffb3
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2023-11-28 15:26:30 +0300

    added sorting by alphabetical order in result output

--- FileReader/obj/Debug/net8.0/FileReader.AssemblyInfo.cs
+++ FileReader/obj/Debug/net8.0/FileReader.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileReader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1fef808cb2415d44ef41e0c979fde774f57f272")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c5f94c038e7b7a6fc34030a4c855ff2b9912db")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileReader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileReader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
